1899a5179efa77e68a70daa54fca4ce3a3a84b85
031Iansz <iansilvasaldanha@gmail.com>
2025-07-30 14:01:26 -0300

    Update home.tsx

--- features/home/components/home.tsx
+++ features/home/components/home.tsx
@@ -47,13 +47,14 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center ">
                     <div>
                         <Image
                             src={"/svg/metodiza.svg"}
                             alt="logo"
                             width={logoSize}
                             height={logoSize}
+                            className="transition-transform duration-250 hover:scale-110 cursor-pointer"
                         />
                         <p className="text-center text-[#D23579] font-bold text-4xl mt-5 text-shadow-[4px_4px] text-shadow-black">
                             Metodiza
@@ -62,7 +63,7 @@ export default function Home() {
                 </div>
             </div>
             <div className="py-10 bg-surface">
-                <div className="w-full px-52 flex justify-around">
+                <div className="w-full px-52 flex justify-around space-x-6">
                     <div className="overview-card">
                         <PenSquare size={cardIconSize} className="my-auto" />
                         <p className="text-xl leading-8">
@@ -72,7 +73,7 @@ export default function Home() {
                     </div>
                     <div className="overview-card">
                         <Clock size={cardIconSize} className="my-auto" />
-                        <p className="text-xl leading-8">
+                        <p className="text-xl leading-8 ">
                             Organize seu tempo Faça a sua rotina Planeje seus
                             estudos
                         </p>
@@ -82,6 +83,104 @@ export default function Home() {
                         <p className="text-xl leading-8">
                             Decida a forma na qual você irá aplicar seus
                             conhecimentos
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="p-10 bg-surface space-y-24">
+                <div className="mx-auto max-w-[1300px] grid gap-12 grid-cols-2">
+                    <div className="mx-auto">
+                        <h2 className="text-[#35C0D2] font-bold text-3xl mb-2 ">
+                            Pomodiza
+                        </h2>
+                        <p className="text-md leading-6">
+                            Dê resumo de estudos à criação de mapas mentais e
+                            projetos,<br></br> com ferramentas essenciais da
+                            nossa plataforma. A técnica<br></br> Pomodoro é uma
+                            estratégia simples para melhorar seu foco<br></br> e
+                            sua produtividade. Ela divide seu tempo em blocos de
+                            trabalho<br></br> focado, intercalados com pequenas
+                            pausas, ajudando você a,<br></br>
+                            manter a concentração por mais tempo.
+                        </p>
+                    </div>
+                    <div className="mx-auto">
+                        <Image
+                            src="/jpg/pomodoro.jpg"
+                            alt="Pomodoro"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </div>
+
+                <div className="mx-auto max-w-[1300px] grid gap-12 grid-cols-2">
+                    <div className="mx-auto">
+                        <Image
+                            src="/jpg/dicionario.jpg"
+                            alt="Dicionário"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                    <div className="mx-auto">
+                        <h2 className="text-[#35C0D2] font-bold text-3xl mb-2">
+                            Dicionário
+                        </h2>
+                        <p className="text-md leading-6">
+                            Um dicionário é uma ferramenta que reúne palavras e
+                            <br></br>
+                            seus significados, organizadas de forma clara e
+                            acessível.<br></br> Ele serve para esclarecer
+                            dúvidas sobre termos, ortografia,<br></br> pronúncia
+                            ou usos corretos. É essencial para ampliar o
+                            vocabulário<br></br> e melhorar a compreensão de
+                            textos.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="mx-auto max-w-[1300px] grid gap-12 grid-cols-2">
+                    <div className="mx-auto">
+                        <h2 className="text-[#35C0D2] font-bold text-3xl mb-2 ">
+                            Grafo
+                        </h2>
+                        <p className="text-md leading-6">
+                            O grafo é um abiente onde todos os temas estudados
+                            estão<br></br> prensentes, organizados de forma única e
+                            encorajadora.<br></br> Motivando o úsuario a cada vez mais
+                            estudar para completar<br></br> e aumentar seu grafo.
+                        </p>
+                    </div>
+                    <div className="mx-auto">
+                        <Image
+                            src="/jpg/grafo.jpg"
+                            alt="Grafo"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </div>
+
+                <div className="mx-auto max-w-[1300px] grid gap-12 grid-cols-2">
+                    <div className="mx-auto">
+                        <Image
+                            src="/jpg/calendario.jpg"
+                            alt="Calendário"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                    <div className="mx-auto">
+                        <h2 className="text-[#35C0D2] font-bold text-3xl mb-2">
+                            Calendário
+                        </h2>
+                        <p className="text-md leading-6">
+                            Um calendário é um sistema de organização do
+                            tempo,dividido<br></br> em dias, semanas, meses e anos. Ele
+                            serve para planejar e registrar<br></br> eventos,
+                            compromissos e datas importantes. Facilita o
+                            controle de<br></br> prazos, rotinas e atividades pessoais
                         </p>
                     </div>
                 </div>
